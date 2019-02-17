6ed0aafefb3af0f13ffe9f25f827d4c94e94b888
XSA_DEV <rahul4sap@gmail.com>
2019-02-17 06:52:09 +0000

    Hana Basics - 9.4

---
+++ srv/router/routes/myNode.js
@@ -0,0 +1,18 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-undef:0, no-process-exit:0, new-cap:0*/
+/*eslint-env node, es6 */
+
+"use strict";
+
+var express = require("express");
+var async = require("async");
+
+module.exports = function() {
+	var app = express.Router();
+	
+	//Hello Router
+	app.get("/Hi", (req, res) => {
+		res.send("Hello World Node.js");
+	});
+	
+	return app;
+};
