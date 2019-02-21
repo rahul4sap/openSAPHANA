309c48e35aeac0ebefc5c6ef7c71b3b5c8867b1d
XSA_DEV <rahul4sap@gmail.com>
2019-02-21 16:35:11 +0000

    Hana Basics - 49th Video

--- srv/router/index.js
+++ srv/router/index.js
@@ -9,4 +9,11 @@ module.exports = (app, server) => {
 	app.use("/node/os/web", express.static("os_web"));
 	app.use("/node/os", require("./routes/os")());
 	app.use("/node/excAsync", require("./routes/exercisesAsync")(server));
+	app.use("/node/JavaScriptBasics", require("./routes/JavaScriptBasics")());
+	app.use("/node/textBundle", require("./routes/textBundle")());
+	app.use("/node/excel", require("./routes/excel")());
+	app.use("/node/xml", require("./routes/xml")());
+	app.use("/node/zip", require("./routes/zip")());
+	app.use("/node/oo", require("./routes/oo")());
+	app.use("/node/chat", require("./routes/chatServer")(server));
 };
